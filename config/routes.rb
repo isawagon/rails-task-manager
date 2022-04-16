c71034286e90fe6ce15eea4b68e73e232dd9ff51
isabelle levy <isabel.levy@icloud.com>
2022-04-16 13:06:53 +0200

    show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'index', to: 'tasks#index#'
+  # get 'index', to: 'tasks#index#'
+  get 'tasks' => 'tasks#index'
+  get 'tasks/:id' => 'tasks#show#', as: :task
 end
