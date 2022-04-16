e8fa9434624833d8958a9e9c166178dd3fde4d92
isabelle levy <isabel.levy@icloud.com>
2022-04-16 14:35:40 +0200

    create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,18 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+  def new
+    @task = Task.new
+  end
+  def create
+    # Task.create(params[:task]) # remplacé ci-dessous pour securité
+    Task.create(task_params)
+    redirect_to tasks_path # ou redirect_to task_path(task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'index', to: 'tasks#index#'
+  # lire toutes les taches
   get 'tasks' => 'tasks#index'
+  # create a task
+  get 'tasks/new' => 'tasks#new'
+  post 'tasks' => 'tasks#create'
+  # lire 1 tache
   get 'tasks/:id' => 'tasks#show#', as: :task
+
+
 end
