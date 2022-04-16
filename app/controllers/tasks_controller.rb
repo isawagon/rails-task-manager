a1068c044c1b1a988743a950225d574410c5c29d
isabelle levy <isabel.levy@icloud.com>
2022-04-16 16:14:14 +0200

    delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,36 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
   def new
     @task = Task.new
   end
+
   def create
     # Task.create(params[:task]) # remplacé ci-dessous pour securité
     Task.create(task_params)
     redirect_to tasks_path # ou redirect_to task_path(task)
   end
+
   def edit
     @task = Task.find(params[:id])
   end
+
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'index', to: 'tasks#index#'
   # lire toutes les taches
-  get 'tasks' => 'tasks#index'
+  get 'tasks'           => 'tasks#index'
   # create a task
-  get 'tasks/new' => 'tasks#new', as: :new_task
-  post 'tasks' => 'tasks#create'
+  get 'tasks/new'       => 'tasks#new',   as: :new_task
+  post 'tasks'          => 'tasks#create'
+  # destroy a task
+  delete 'tasks/:id'    => 'tasks#destroy'
   # lire 1 tache
-  get 'tasks/:id' => 'tasks#show#', as: :task
+  get 'tasks/:id'       => 'tasks#show#', as: :task
   # edit a task
-  get 'tasks/:id/edit' => 'tasks#edit', as: :edit_task
-  patch 'tasks/:id' => 'tasks#update'
+  get 'tasks/:id/edit'  => 'tasks#edit',  as: :edit_task
+  patch 'tasks/:id'     => 'tasks#update'
 end
